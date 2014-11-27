7163463ca35b91ccdfd88f18f5f9016c8221361c
Philippe Marschall <philippe.marschall@gmail.com>
2014-11-27 10:30:38 +0100

    start with dagger example

--- build-dagger-example.sh
+++ build-dagger-example.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+MAVEN_OPTS="-Xms32m -Xmx32m -Xss160k -Xnoverify"
+
 H2_VERSION=1.4.182
 
-# Make sure the dependency is in the local repo
-mvn dependency:get -Dartifact=com.h2database:h2:${H2_VERSION}
+DAGGER_EXAMPLE_VERSION=1.0.0-SNAPSHOT
+
+OUTPUT_FOLDER=dagger-example/dependencies
+
+if [ ! -f ~/.m2/repository/com/github/marschall/dagger-example-web/${DAGGER_EXAMPLE_VERSION}/dagger-example-web-${DAGGER_EXAMPLE_VERSION}.war ]
+  then
+    echo build and install dagger-example-web first
+    exit 1
+  else
+    cp ~/.m2/repository/com/github/marschall/dagger-example-web/${DAGGER_EXAMPLE_VERSION}/dagger-example-web-${DAGGER_EXAMPLE_VERSION}.war ${OUTPUT_FOLDER}/ROOT.war
+fi
+
+if [ ! -f dagger-example/dependencies/h2-${H2_VERSION}.jar ]
+  then
+    mvn dependency:get -Dartifact=com.h2database:h2:${H2_VERSION}
+    mvn dependency:copy -Dartifact=com.h2database:h2:${H2_VERSION} -DoutputDirectory=$OUTPUT_FOLDER
+fi
+
+if [ ! -f dagger-example/dependencies/null-jarscanner-1.0.0.jar ]
+  then
+    mvn dependency:get -Dartifact=com.github.marschall:null-jarscanner:1.0.0
+    mvn dependency:copy -Dartifact=com.github.marschall:null-jarscanner:1.0.0 -DoutputDirectory=$OUTPUT_FOLDER
+fi
+
 
 docker build -t marschall/dagger-example --rm dagger-example
---
+++ dagger-example/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (employee_id, first_name, last_name, date_of_birth    , phone_number)
+       VALUES         (1          , 'John'    , 'Doe'    , DATE '1980-01-01', '1234567890');
