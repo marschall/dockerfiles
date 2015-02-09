f856cc4f54a0f1c22e690a361aefbb8602b8350b
Philippe Marschall <philippe.marschall@gmail.com>
2015-02-09 12:46:10 +0100

    bump Oracle JRE parameterize build script

--- build-oracle-server-jre.sh
+++ build-oracle-server-jre.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 JAVA_VERSION_MAJOR=8
-JAVA_VERSION_MINOR=25
-JAVA_VERSION_BUILD=12
+JAVA_VERSION_MINOR=31
+JAVA_VERSION_BUILD=13
 
 if [ ! -f "oracle-server-jre/server-jre-8u25-linux-x64.tar.gz" ]
   then
-    curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u25-b17/server-jre-8u25-linux-x64.tar.gz
-    mv server-jre-8u25-linux-x64.tar.gz oracle-server-jre
+    curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-b${JAVA_VERSION_BUILD}/server-jre-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz"
+    mv "server-jre-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz" oracle-server-jre
 fi
 
 if [ ! -f "oracle-server-jre/jce_policy-8.zip" ]
