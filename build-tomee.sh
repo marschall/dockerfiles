22ca4a449987a9fd4e8ba615101d071bb84e7ae0
Philippe Marschall <philippe.marschall@gmail.com>
2015-03-18 17:20:38 +0100

    add tomee

---
+++ build-tomee.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t marschall/apache-tomee:1 --rm apache-tomee/1
+
---
+++ run-tomee.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -it --rm  marschall/apache-tomee:1 /opt/apache-tomee/bin/version.sh
+
