5ac47283d4362e336d8cdf35d7d8968c9c8ce6f6
Philippe Marschall <philippe.marschall@gmail.com>
2015-03-18 16:17:18 +0100

    add Glassfish

---
+++ build-glassfish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t marschall/glassfish:4 --rm glassfish/4 
+
---
+++ run-glassfish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -it --rm  marschall/glassfish:4 /opt/glassfish/bin/asadmin version 
+ 
