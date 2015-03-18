1570979a1fbbbbc20e85183d9d6ca31728d65375
Philippe Marschall <philippe.marschall@gmail.com>
2015-03-18 07:38:10 +0100

    bump java to 8 update 40

--- build-oracle-server-jre.sh
+++ build-oracle-server-jre.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 JAVA_VERSION_MAJOR=8
-JAVA_VERSION_MINOR=31
-JAVA_VERSION_BUILD=13
+JAVA_VERSION_MINOR=40
+JAVA_VERSION_BUILD=26
 
 if [ ! -f "oracle-server-jre/server-jre-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz" ]
   then
