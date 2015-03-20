7bc1d2fad6e152c1f6351c09e9c33643b9b21e8a
Philippe Marschall <philippe.marschall@gmail.com>
2015-03-20 08:21:10 +0100

    have jetty running

--- jdt-availability-check/build-all.sh
+++ jdt-availability-check/build-all.sh
@@ -15,15 +15,19 @@ if [ ! -f ~/.m2/repository/com/github/marschall/jdt-availability-check/${PROJECT
     cp ~/.m2/repository/com/github/marschall/jdt-availability-check/${PROJECT_VERSION}/jdt-availability-check-${PROJECT_VERSION}.war glassfish/dependencies/jdt-availability-check.war
     cp ~/.m2/repository/com/github/marschall/jdt-availability-check/${PROJECT_VERSION}/jdt-availability-check-${PROJECT_VERSION}.war  geronimo/dependencies/jdt-availability-check.war
     cp ~/.m2/repository/com/github/marschall/jdt-availability-check/${PROJECT_VERSION}/jdt-availability-check-${PROJECT_VERSION}.war     resin/dependencies/jdt-availability-check.war
+    cp ~/.m2/repository/com/github/marschall/jdt-availability-check/${PROJECT_VERSION}/jdt-availability-check-${PROJECT_VERSION}.war     jetty/dependencies/jdt-availability-check.war
+
 
 fi
 
-docker build -t marschall/jdt-availability-check-tomcat    --rm tomcat
-docker build -t marschall/jdt-availability-check-tomee     --rm tomee
-docker build -t marschall/jdt-availability-check-wildfly   --rm wildfly
-docker build -t marschall/jdt-availability-check-glassfish --rm glassfish
-docker build -t marschall/jdt-availability-check-geronimo  --rm geronimo
-docker build -t marschall/jdt-availability-check-resin     --rm resin
+#docker build -t marschall/jdt-availability-check-tomcat    --rm tomcat
+#docker build -t marschall/jdt-availability-check-tomee     --rm tomee
+#docker build -t marschall/jdt-availability-check-wildfly   --rm wildfly
+#docker build -t marschall/jdt-availability-check-glassfish --rm glassfish
+#docker build -t marschall/jdt-availability-check-geronimo  --rm geronimo
+#docker build -t marschall/jdt-availability-check-resin     --rm resin
+docker build -t marschall/jdt-availability-check-jetty     --rm jetty
+
 
 
 
