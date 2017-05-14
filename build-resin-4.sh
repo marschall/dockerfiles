d00d139fcd8360c6d500840f0ef5de72dee52582
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-14 20:04:56 +0200

    update resin

--- build-resin-4.sh
+++ build-resin-4.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker build -t marschall/resin:4 --rm resin/4 
+RESIN_VERSION=4.0.51
+
+if [ ! -f "resin/4/resin-${RESIN_VERSION}.tar.gz" ]
+  then
+    curl -LO "http://caucho.com/download/resin-${RESIN_VERSION}.tar.gz"
+    mv "resin-${RESIN_VERSION}.tar.gz" resin/4
+fi
+
+docker build -t marschall/resin:4 --rm resin/4
 
