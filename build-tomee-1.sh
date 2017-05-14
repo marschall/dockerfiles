827f62236c6d4abd12f75c607e66d25a712a959e
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-14 18:23:02 +0200

    update tomee

--- build-tomee-1.sh
+++ build-tomee-1.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+TOMEE_VERSION=1.7.4
+TOMEE_PROFILE=webprofile
+
+
+if [ ! -f "apache-tomee/1/apache-tomee-${TOMEE_VERSION}-${TOMEE_PROFILE}.tar.gz" ]
+  then
+    curl -LO "http://repo.maven.apache.org/maven2/org/apache/openejb/apache-tomee/${TOMEE_VERSION}/apache-tomee-${TOMEE_VERSION}-${TOMEE_PROFILE}.tar.gz"
+    mv "apache-tomee-${TOMEE_VERSION}-${TOMEE_PROFILE}.tar.gz" apache-tomee/1
+fi
+
 docker build -t marschall/apache-tomee:1 --rm apache-tomee/1
 
---
+++ run/run-tomee-7.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -it --rm  marschall/apache-tomee:7 /opt/apache-tomee/bin/version.sh
+
