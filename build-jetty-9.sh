db9e6f6d1192978ea6d7ce946502834414474fdb
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-14 20:53:08 +0200

    update jetty

--- build-jetty-9.sh
+++ build-jetty-9.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker build -t marschall/jetty:9 --rm jetty/9 
+JETTY_BRANCH=stable-9
+JETTY_VERSION=9.4.5.v20170502
+
+if [ ! -f "jetty/9/jetty-distribution-${JETTY_VERSION}.tar.gz" ]
+  then
+    curl -LO "http://central.maven.org/maven2/org/eclipse/jetty/jetty-distribution/${JETTY_VERSION}/jetty-distribution-${JETTY_VERSION}.tar.gz"
+    mv "jetty-distribution-${JETTY_VERSION}.tar.gz" jetty/9
+fi
+
+docker build -t marschall/jetty:9 --rm jetty/9
 
--- run/run-jetty-9.sh
+++ run/run-jetty-9.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it --rm  marschall/jetty:9 /opt/jetty/bin/jetty.sh check
-# docker run -it --rm  marschall/jetty:9 java -Djetty.logs=/opt/jetty/logs -Djetty.home=/opt/jetty -Djetty.base=/opt/jetty -jar /opt/jetty/start.jar jetty.state=/opt/jetty/jetty.state jetty-logging.xml jetty-started.xml
+docker run -it --rm marschall/jetty:9 /opt/jetty/bin/jetty.sh check
+# docker run -it --rm marschall/jetty:9 java -Djetty.logs=/opt/jetty/logs -Djetty.home=/opt/jetty -Djetty.base=/opt/jetty -jar /opt/jetty/start.jar jetty.state=/opt/jetty/jetty.state jetty-logging.xml jetty-started.xml
  
