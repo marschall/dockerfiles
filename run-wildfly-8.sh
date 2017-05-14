2503261dc2b0d823b18e3befbbd62f69750083e5
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-14 20:54:06 +0200

    remove old wildfly version

--- run-wildfly-8.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm  marschall/wildfly:8 /opt/wildfly/bin/standalone.sh -v
- 
--- run-wildfly-9.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm  marschall/wildfly:9 /opt/wildfly/bin/standalone.sh -v
- 
