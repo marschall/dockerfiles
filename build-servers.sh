fd6788e399e990edbe61391156b61e5c99b21581
Philippe Marschall <philippe.marschall@gmail.com>
2017-05-15 21:14:46 +0200

    update servers

--- build-servers.sh
+++ build-servers.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-./build-tomcat.sh && \
-./build-tomee.sh && \
-./build-resin.sh && \
+./build-tomcat-8.sh && \
+./build-tomcat-9.sh && \
+./build-tomee-1.sh && \
+./build-tomee-7.sh && \
+./build-resin-4.sh && \
 ./build-geronimo.sh && \
-./build-glassfish.sh && \
-./build-jetty.sh && \
-./build-wildfly-8.sh && \
-./build-wildfly-9.sh && \
+./build-glassfish-4.sh && \
+./build-glassfish-5.sh && \
+./build-payara-4.sh && \
+./build-jetty-9.sh && \
 ./build-wildfly-10.sh && \
+./build-wildfly-11.sh && \
 ./build-dagger-example.sh
 
