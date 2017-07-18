f1f9795bb1daf36b22b2e149bd3635923b587332
Philippe Marschall <philippe.marschall@gmail.com>
2017-07-18 13:03:33 +0200

    move openjdk builds to different repo

--- build-openjdk-10.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t marschall/build-openjdk:10 build-openjdk/10
-
--- run-build-jdk10.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir jdk10
-
-docker run -ti --rm -v `pwd -P`/jdk10:/jdk marschall/build-openjdk:10
-
