770525b68a4c210fd3d6f5c48bb283b0a4fb6bdb
Philippe Marschall <philippe.marschall@gmail.com>
2017-09-24 14:38:29 +0200

    JDK 8 simplifications

--- build-oracle-server-jre-8.sh
+++ build-oracle-server-jre-8.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -e
 
-JAVA_VERSION_MAJOR=8
 JAVA_VERSION_MINOR=144
 JAVA_VERSION_BUILD=01
 JAVA_VERSION_UUID=090f390dda5b47b9b721c7dfaa008135
 
-if [ ! -f "oracle-server-jre/8/server-jre-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz" ]
+if [ ! -f "oracle-server-jre/8/server-jre-8u${JAVA_VERSION_MINOR}-linux-x64.tar.gz" ]
   then
-    curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-b${JAVA_VERSION_BUILD}/${JAVA_VERSION_UUID}/server-jre-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz"
-    mv "server-jre-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_MINOR}-linux-x64.tar.gz" oracle-server-jre/8
+    curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u${JAVA_VERSION_MINOR}-b${JAVA_VERSION_BUILD}/${JAVA_VERSION_UUID}/server-jre-8u${JAVA_VERSION_MINOR}-linux-x64.tar.gz"
+    mv "server-jre-8u${JAVA_VERSION_MINOR}-linux-x64.tar.gz" oracle-server-jre/8
 fi
 
-if [ ! -f "oracle-server-jre/8/jce_policy-${JAVA_VERSION_MAJOR}.zip" ]
+if [ ! -f "oracle-server-jre/8/jce_policy-8.zip" ]
   then
-    curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/${JAVA_VERSION_MAJOR}/jce_policy-${JAVA_VERSION_MAJOR}.zip
+    curl -LOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
     mv jce_policy-8.zip oracle-server-jre/8
     unzip -d oracle-server-jre/8 oracle-server-jre/8/jce_policy-8.zip
 fi
